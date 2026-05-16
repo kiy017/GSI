eb95c45fba780b9b79dec6399bd2bde457ed6c95
rofikkernel <rofikcoga@gmail.com>
2026-05-16 11:49:35 +0700

    readded back

--- LinkToGSI.sh
+++ LinkToGSI.sh
@@ -12,12 +12,12 @@ fi
 
 partitions="vendor system system_ext product optics prism mi_ext my_bigball my_engineering my_manifest my_region my_carrier my_heytap my_product my_stock"
 
-#if [[ -d "Tools/Firmware_extractor" ]]; then
- #   echo "already..."
-#else
- #   echo "Cloning Firmware_extractor..."
-  #  git clone -q --recurse-submodules https://github.com/erfanoabdi/Firmware_extractor.git "Tools"/#Firmware_extractor
-#fi
+if [[ -d "Tools/Firmware_extractor" ]]; then
+    echo "already..."
+else
+    echo "Cloning Firmware_extractor..."
+    git clone -q --recurse-submodules https://github.com/erfanoabdi/Firmware_extractor.git "Tools"/Firmware_extractor
+fi
 
 usage() {
   echo "Usage: $0 [rom_link] [rom_type]"
